239b4368277ee6230b317b875a55335d361990c2
Barrett K. Harber <barrett.harber@gmail.com>
2016-07-07 17:03:14 -0400

    Update build for images dir

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ LOCAL=icons.zip
 REMOTE=https://media.amazonwebservices.com/AWS-Design/Arch-Center/16.2.22_Update/AWS_Simple_Icons_EPS-SVG_v16.2.22.zip
 
 git checkout gh-pages \
-	&& rm -rf * 2> /dev/null \
+	&& rm -rf images/* 2> /dev/null \
 	&& curl -o $LOCAL $REMOTE \
-	&& unzip -j $LOCAL \
+	&& unzip -j $LOCAL -d images/ -x *._* \
 	&& rm $LOCAL \
-	&& git add . \
-	&& git commit -am "Build: $(date -u)"
+	&& git add images/ \
+	&& git commit -m "Build: $(date -u)"
